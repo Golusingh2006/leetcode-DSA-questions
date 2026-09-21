0efc885f7a7787dfb5eebf7c46a1c415f474bc8e
GOLU KUMAR <159444003+Golusingh2006@users.noreply.github.com>
2026-09-22 01:59:50 +0530

    Time: 0 ms (100%), Space: 8.8 MB (3.58%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
 
-        // Overflow case
+      
         if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
 
